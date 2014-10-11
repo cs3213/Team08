00bf1cc09efdc791181c531dabaac44fc7b3d61b
Wee Loon <weeloonc@outlook.com>
2014-10-11 20:31:14 +0800

    Added CommandFactory service

---
+++ app/scripts/services/command-factory.js
@@ -0,0 +1,63 @@
+'use strict';
+
+angular.module('rebroApp')
+
+    .factory('CommandFactory', function(Character) {
+        var constructors = {
+            setX: function(receiver, value) {
+                this.execute = function() {
+                    receiver.xPos = value;
+                }
+            },
+
+            setY: function(receiver, value) {
+                this.execute = function() {
+                    receiver.yPos = value;
+                }
+            },
+
+            moveX: function(receiver, value) {
+                this.execute = function() {
+                    receiver.xPos += value;
+                }
+            },
+
+            moveY: function(receiver, value) {
+                this.execute = function() {
+                    receiver.yPos += value;
+                }
+            },
+
+            changeCostume: function(receiver, value) {
+                this.execute = function() {
+                    receiver.costume = value;
+                }
+            },
+
+            show: function(receiver) {
+                this.execute = function() {
+                    receiver.isVisible = true;
+                }
+            },
+
+            hide: function(receiver) {
+                this.execute = function() {
+                    receiver.isVisible = false;
+                }
+            }
+        };
+
+        return {
+            createCommand: function(name, receiver, args) {
+
+                if (!constructors.hasOwnProperty(name)) {
+                    throw new Error('Command "' + name + '" is undefined');
+                }
+
+                var command = {};
+                constructors[name].apply(command, [receiver].concat(args));
+
+                return command;
+            }
+        };
+    });
