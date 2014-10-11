e06abc796f6cb73f717ff071ba8fcb57b0434edb
Wee Loon <weeloonc@outlook.com>
2014-10-11 20:33:18 +0800

    Added Program, Statement, Character constructors

---
+++ app/scripts/app-classes.js
@@ -0,0 +1,24 @@
+'use strict';
+
+angular.module('rebroApp')
+
+    .value('Program', function() {
+        this.stmtList = [];
+    })
+
+    .value('Statement', function(type) {
+        this.type = type;
+        this.args = [];
+        this.stmtList = [];
+
+        for (var i = 1; i < arguments.length; i++) {
+            this.args.push(arguments[i]);
+        }
+    })
+
+    .value('Character', function() {
+        this.xPos = 0;
+        this.yPos = 0;
+        this.costume = "default-url";
+        this.isVisible = true;
+    });
