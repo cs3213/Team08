da73ab1a3169be53f61b2bbc47d557989704f414
Wee Loon <weeloonc@outlook.com>
2014-11-04 00:54:55 +0800

    Added StatementRepository service

---
+++ app/scripts/services/statement-repository.js
@@ -0,0 +1,22 @@
+'use strict';
+
+angular.module('rebroApp')
+
+    .factory('StatementRepository', function (Statement) {
+        var templates = [
+            new Statement('setX', false, 0),
+            new Statement('setY', false, 0),
+            new Statement('moveX', false, 0),
+            new Statement('moveY', false, 0),
+            new Statement('hide', false),
+            new Statement('show', false),
+            new Statement('changeCostume', false, 'costume-brainy'),
+            new Statement('repeat', true, '1')
+        ];
+
+        return {
+            getStatementTemplates: function() {
+                return angular.copy(templates);
+            }
+        };
+    });
