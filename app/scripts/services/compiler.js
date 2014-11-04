af6d6b08506b962dd0482d7bf341098dbc07f2c1
Wee Loon <weeloonc@outlook.com>
2014-11-04 13:53:13 +0800

    Updated Compiler service to compile forever-loop

--- app/scripts/services/compiler.js
+++ app/scripts/services/compiler.js
@@ -3,21 +3,46 @@
 angular.module('rebroApp')
     .factory('CompilerSvc', function(CommandFactory) {
 
-        function compileRecursively(numTimes, stmtList, receiver, execList) {
+        var foreverIndex;
+        var foreverNest;
+
+        function compileRecursively(numTimes, stmtList, receiver, execList, nestLevel) {
 
             for (var i = 0; i < numTimes; i++) {
                 for (var j = 0; j < stmtList.length; j++) {
-                    compileStatement(stmtList[j], receiver, execList);
+                    compileStatement(stmtList[j], receiver, execList, nestLevel);
+
+                    if (nestLevel <= foreverNest) {
+                        break;;
+                    }
                 }
             }
 
             return execList;
         }
 
-        function compileStatement(stmt, receiver, execList) {
+        function compileForever(stmtList, receiver, execList, nestLevel) {
+
+            for (var j = 0; j < stmtList.length; j++) {
+                compileStatement(stmtList[j], receiver, execList, nestLevel);
+
+                if (nestLevel < foreverNest) {
+                    break;
+                }
+            }
+
+            return execList;
+        }
+
+        function compileStatement(stmt, receiver, execList, nestLevel) {
 
             if (stmt.type === 'repeat') {
-                compileRecursively(stmt.args[0], stmt.stmtList, receiver, execList);
+                compileRecursively(stmt.args[0], stmt.stmtList, receiver, execList, nestLevel + 1);
+
+            } else if (stmt.type === 'forever') {
+                foreverIndex = execList.length;
+                foreverNest = nestLevel + 1;
+                compileForever(stmt.stmtList, receiver, execList, nestLevel + 1);
 
             } else {
                 var command = CommandFactory.createCommand(stmt.type, receiver, stmt.args);
@@ -27,7 +52,17 @@ angular.module('rebroApp')
 
         return {
             compile: function(program, receiver) {
-                return compileRecursively(1, program.stmtList, receiver, []);
+                foreverNest = -1;
+                foreverIndex = -1;  // no forever looping initially
+
+                var build = {};
+                build.executableList = compileRecursively(1, program.stmtList, receiver, [], 0);
+                build.foreverIndex = foreverIndex;
+                build.loopsForever = function() {
+                    return foreverIndex >= 0;
+                };
+
+                return build;
             }
         };
     });
