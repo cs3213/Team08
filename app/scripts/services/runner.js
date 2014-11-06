caa2596f6b5656cd48bf1d782b996687ff30579e
Wee Loon <weeloonc@outlook.com>
2014-11-07 00:58:50 +0800

    Updated Runner service

--- app/scripts/services/runner.js
+++ app/scripts/services/runner.js
@@ -5,20 +5,74 @@ angular.module('rebroApp')
     .factory('Runner', function($interval) {
 
         var refreshRate = 20;
+        var isRunning = false;
+        var stack = [];
+
+        function StatementPointer(stmtList, repeatCount) {
+            this.stmtList = stmtList;
+            this.index = 0;
+            this.repeatsLeft = repeatCount - 1;
+            this.repeatsForever = repeatCount < 0;
+        }
+
+        function peekStack() {
+            if (stack.length === 0) {
+                return null;
+            } else {
+                return stack[stack.length - 1];
+            }
+        }
 
         return {
-            runProgram: function(build) {
-                var i = 0;
-                var intervalPromise = $interval(function () {
-                    if (i < build.executableList.length) {
-                        build.executableList[i++].execute();
+            runProgram: function(executable) {
+                if (isRunning) {
+                    return;
+                }
 
-                    } else if (build.loopsForever && !build.hasEmptyForever()) {
-                        i = build.foreverIndex;
-                        build.executableList[i++].execute();
+                isRunning = true;
+                stack = [];
+                stack.push(new StatementPointer(executable.stmtList, 0));
+
+                var intervalPromise = $interval(function () {
+                    var ptr = peekStack();
+
+                    if (ptr.index < ptr.stmtList.length) {
+                        var stmt = ptr.stmtList[ptr.index];
+
+                        if (stmt.type === 'repeat') {
+                            var repeatCount = stmt.args[0];
+                            stack.push(new StatementPointer(stmt.stmtList, repeatCount));
+                            //redo
+
+                        } else if (stmt.type === 'forever') {
+                            stack.push(new StatementPointer(stmt.stmtList, -1));
+                            //redo
+
+                        //} else if (stmt.type === 'if') {
+
+
+                        } else {
+                            stmt.command.execute();
+                        }
+
+                        ptr.index++;
+
+                    } else if (ptr.repeatsLeft > 0) {
+                        ptr.index = 0;
+                        ptr.repeatsLeft--;
+                        //redo
+
+                    } else if (ptr.repeatsForever) {
+                        ptr.index = 0;
+                        //redo
+
+                    } else if (stack.length > 1) {
+                        stack.pop();
+                        //redo
 
                     } else {
                         $interval.cancel(intervalPromise);
+                        isRunning = false;
                     }
                 }, refreshRate);
 
@@ -27,6 +81,7 @@ angular.module('rebroApp')
 
             stopProgram: function(intervalPromise) {
                 $interval.cancel(intervalPromise);
+                isRunning = false;
             }
         };
 
